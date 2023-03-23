ea4acd6c90808963e346562f9881641b71e79a52
kummersh <kummersh@gmail.com>
2023-03-23 12:50:16 +0200

    Added two new HTML files

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template, request
-app = Flask(__name__, static_url_path='/static')
+
+import MemoryGame
+from MemoryGame import generate_sequence
+app = Flask(__name__)
+
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('chooseGame.html')
 
 
-@app.route('/game', methods=['POST'])
-def game():
+@app.route('/play', methods=['POST'])
+def play():
     choice = int(request.form['game'])
     level = int(request.form['level'])
-    if choice == 1:
-        return render_template('memorygame.html', level=level)
 
-    # Add code for other games here
-    return 'Game Over'
+    if choice == 1:
+        random_numbers = MemoryGame.generate_sequence(level)
+        return render_template('memoryGame.html', level=level, numbers=random_numbers)
 
 
 if __name__ == '__main__':
