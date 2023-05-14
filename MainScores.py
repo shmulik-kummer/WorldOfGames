965485408ab61ebf6cf31c9f206da3a6b40d64dc
shmulik-kummer <kummersh@gmail.com>
2023-05-14 21:51:22 +0300

    Add new file - MainScores.py

---
+++ MainScores.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import Utils
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def score_server():
+    try:
+        with open(Utils.SCORES_FILE_NAME, 'r') as f:
+            score = f.read()
+        score_div = f'<div id="score">{score}</div>'
+    except Exception as e:
+        score_div = f'<div id="score" style="color:red">{str(e)}</div>'
+
+    html = f"""<html>
+<head>
+<title>Scores Game</title>
+</head>
+<body>
+<h1>The score is {score_div}</h1>
+</body>
+</html>"""
+    return html
+
+
+app.run()
