4f67fafab24ea8046625327bb2080ea850a68c67
shmulik-kummer <kummersh@gmail.com>
2023-05-15 08:49:25 +0300

    Refactor Score.py file

--- Score.py
+++ Score.py
@@ -1,22 +1,29 @@
 import Utils
 
 
-def add_score(difficulty):
-    file_name = Utils.SCORES_FILE_NAME
+def read_current_score(file_name):
     try:
-        # Try to read the current score from the file
         with open(file_name, 'r') as f:
             current_score = int(f.read())
             print(f"Previous total score: {current_score}")
+            return current_score
     except FileNotFoundError:
-        # If the file doesn't exist, create a new one with a starting score of 0
-        current_score = 0
-        with open(file_name, 'w') as f:
-            f.write(str(current_score))
+        print("No previous score found.")
+        return 0
 
-    # Calculate the new score and update the file
-    new_score = current_score + ((difficulty * 3) + 5)
-    print(f"Updated total score is: {new_score}")
+
+def write_new_score(file_name, new_score):
     with open(file_name, 'w') as f:
         f.write(str(new_score))
+    print(f"Updated total score is: {new_score}")
 
+
+def add_score(difficulty):
+    # Get the file name
+    file_name = Utils.SCORES_FILE_NAME
+    # Read current score
+    current_score = read_current_score(file_name)
+    # Calculate new score
+    new_score = current_score + (difficulty * 3 + 5)
+    # Update new score
+    write_new_score(file_name, new_score)
