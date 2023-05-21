f07b269291d39b84044173794b1d4566d650a97f
shmulik-kummer <kummersh@gmail.com>
2023-05-21 12:49:59 +0300

    Add new file - Live.py

---
+++ Live.py
@@ -0,0 +1,5 @@
+def welcome(name: str) -> str:
+    """
+    This function takes a person's name as an input and returns a string welcoming them to the World of Games.
+    """
+    return f"Hello {name} and welcome to the World of Games (WoG). Here you can find many cool games to play."
