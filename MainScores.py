9d598ac26d257dd993918091d03ae9983870b07f
shmulik-kummer <kummersh@gmail.com>
2023-05-22 18:02:04 +0300

    Separate html in MainScores.py to file

--- MainScores.py
+++ MainScores.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import Utils
 
 app = Flask(__name__)
@@ -9,19 +9,11 @@ def score_server():
     try:
         with open(Utils.SCORES_FILE_NAME, 'r') as f:
             score = f.read()
-        score_div = f'<div id="score">{score}</div>'
+        score_div = score
     except Exception as e:
-        score_div = f'<div id="score" style="color:red">{str(e)}</div>'
+        score_div = str(e)
 
-    html = f"""<html>
-<head>
-<title>Scores Game</title>
-</head>
-<body>
-<h1>The score is: {score_div}</h1>
-</body>
-</html>"""
-    return html
+    return render_template("/index.html", score_div=score_div)
 
 
 app.run(host="0.0.0.0")
